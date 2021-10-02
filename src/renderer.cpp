b9d1d92d858224e218291afdc909d6e989c07881
Nicolas VIVIEN <nicolas@vivien.fr>
2021-10-02 10:42:16 +0200

    Add label renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -25,13 +25,13 @@ Renderer * Renderer::create(void) {
 
 
 void Renderer::draw(FramePtr frame) {
-	int x, y; //, w, h;
+	int x, y, w, h;
 
 	OIIO::ImageBuf frame_buffer = frame->toImageBuf();
 
 	// Open picto
 //	OIIO::ImageInput *img = OIIO::ImageInput::open("./assets/picto.png");
-	auto img = OIIO::ImageInput::open("./assets/picto/DataOverlay_icn_laps.png");
+	auto img = OIIO::ImageInput::open("./assets/picto/DataOverlay_icn_speed.png");
 	const OIIO::ImageSpec& spec = img->spec();
 	VideoParams::Format img_fmt = OIIOUtils::getFormatFromOIIOBaseType((OIIO::TypeDesc::BASETYPE) spec.format.basetype);
 	OIIO::TypeDesc::BASETYPE type = OIIOUtils::getOIIOBaseTypeFromFormat(img_fmt);
@@ -45,8 +45,8 @@ void Renderer::draw(FramePtr frame) {
 
 	x = 50;
 	y = 900;
-//	w = spec.width;
-//	h = spec.height;
+	w = spec.width * 2.5;
+	h = spec.height * 2.5;
 
 	// Image over
 	dst.specmod().x = x;
@@ -54,6 +54,19 @@ void Renderer::draw(FramePtr frame) {
 	OIIO::ImageBufAlgo::over(frame_buffer, dst, frame_buffer, OIIO::ROI());
 
 	delete buf;
+
+
+	// Add label
+	int pt;
+	int space = h / 10;
+	pt = (h - (3 *space)) / 3;
+	float white[] = { 1.0, 1.0, 1.0, 1.0 };
+	if (OIIO::ImageBufAlgo::render_text(frame_buffer, x + w + (w/10), y + space + pt, "VITESSE", pt, "./assets/fonts/Helvetica.ttf", white) == false)
+		fprintf(stderr, "render text error\n");
+	pt *= 2;
+	if (OIIO::ImageBufAlgo::render_text(frame_buffer, x + w + (w/10), y + (pt/2) + space + pt + space, "32 km/h", pt, "./assets/fonts/Helvetica.ttf", white) == false)
+		fprintf(stderr, "render text error\n");
+
 
 	frame->fromImageBuf(frame_buffer);
 }
