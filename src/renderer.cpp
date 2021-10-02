c95c8645160de11b81ab3cae68ee9b53f0b563b4
Nicolas VIVIEN <nicolas@vivien.fr>
2021-10-02 10:23:05 +0200

    Use picto & rescale

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -31,7 +31,7 @@ void Renderer::draw(FramePtr frame) {
 
 	// Open picto
 //	OIIO::ImageInput *img = OIIO::ImageInput::open("./assets/picto.png");
-	auto img = OIIO::ImageInput::open("./assets/picto.png");
+	auto img = OIIO::ImageInput::open("./assets/picto/DataOverlay_icn_laps.png");
 	const OIIO::ImageSpec& spec = img->spec();
 	VideoParams::Format img_fmt = OIIOUtils::getFormatFromOIIOBaseType((OIIO::TypeDesc::BASETYPE) spec.format.basetype);
 	OIIO::TypeDesc::BASETYPE type = OIIOUtils::getOIIOBaseTypeFromFormat(img_fmt);
@@ -39,15 +39,19 @@ void Renderer::draw(FramePtr frame) {
 	OIIO::ImageBuf *buf = new OIIO::ImageBuf(OIIO::ImageSpec(spec.width, spec.height, spec.nchannels, type)); //, OIIO::InitializePixels::No);
 	img->read_image(type, buf->localpixels());
 
+	// Resize picto
+	OIIO::ImageBuf dst(OIIO::ImageSpec(spec.width * 2.5, spec.height * 2.5, spec.nchannels, type)); //, OIIO::InitializePixels::No);
+	OIIO::ImageBufAlgo::resize(dst, *buf);
+
 	x = 50;
 	y = 900;
 //	w = spec.width;
 //	h = spec.height;
 
 	// Image over
-	buf->specmod().x = x;
-	buf->specmod().y = y;
-	OIIO::ImageBufAlgo::over(frame_buffer, *buf, frame_buffer, OIIO::ROI());
+	dst.specmod().x = x;
+	dst.specmod().y = y;
+	OIIO::ImageBufAlgo::over(frame_buffer, dst, frame_buffer, OIIO::ROI());
 
 	delete buf;
 
