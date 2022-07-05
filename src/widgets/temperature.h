62c189ff09a825a2a3f7c24dfec62543e96c97c0
shajen <shajen@shajen.pl>
2022-07-05 20:21:14 +0200

    Added temperature support.

---
+++ src/widgets/temperature.h
@@ -0,0 +1,59 @@
+#ifndef __GPX2VIDEO__WIDGETS__TEMPERATURE_H__
+#define __GPX2VIDEO__WIDGETS__TEMPERATURE_H__
+
+#include "log.h"
+#include "videowidget.h"
+
+
+class TemperatureWidget : public VideoWidget {
+public:
+	virtual ~TemperatureWidget() {
+		log_call();
+
+		if (buf_)
+			delete buf_;
+	}
+
+	static TemperatureWidget * create(GPX2Video &app) {
+		TemperatureWidget *widget;
+
+		log_call();
+
+		widget = new TemperatureWidget(app, "temperature");
+
+		return widget;
+	}
+
+	void prepare(OIIO::ImageBuf *buf) {
+		this->createBox(&buf_, this->width(), this->height());
+		this->drawBorder(buf_);
+		this->drawBackground(buf_);
+		this->drawImage(buf_, this->border(), this->border(), "./assets/picto/DataOverlay_icn_temperature.png", VideoWidget::ZoomFit);
+
+		// Image over
+		buf_->specmod().x = this->x();
+		buf_->specmod().y = this->y();
+		OIIO::ImageBufAlgo::over(*buf, *buf_, *buf, OIIO::ROI());
+	}
+
+	void render(OIIO::ImageBuf *buf, const GPXData &data) {
+		char s[128];
+
+		sprintf(s, "%.1f %s", data.temperature(), unit2string(unit()).c_str());
+
+		// Append dynamic info
+		this->drawLabel(buf, this->x() + this->height() + this->padding(), this->y(), label().c_str());
+		this->drawValue(buf, this->x() + this->height() + this->padding(), this->y(), s);
+	}
+
+private:
+	OIIO::ImageBuf *buf_;
+
+	TemperatureWidget(GPX2Video &app, std::string name)
+		: VideoWidget(app, name) 
+		, buf_(NULL) {
+	}
+};
+
+#endif
+
